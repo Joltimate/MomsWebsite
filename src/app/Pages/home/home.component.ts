d57c0b1cbbf8d9ceebe1a572999a676abe050454
Joltimate <jultimate69@gmail.com>
2022-05-11 09:52:35 -0400

    added images and updated profile

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SwiperOptions } from 'swiper';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -44,6 +43,111 @@ export class HomeComponent implements OnInit {
     image: 'assets/img/nina5.jpg',
     thumbImage: 'assets/img/nina5.jpg',
   },
+];
+
+hairColor = [{
+  image: 'assets/img/sra7.jpg',
+  thumbImage: 'assets/img/sra7.jpg',
+},
+{
+  image: 'assets/img/sra8.jpg',
+  thumbImage: 'assets/img/sra8.jpg',
+},
+{
+  image: 'assets/img/sra6.jpg',
+  thumbImage: 'assets/img/sra6.jpg',
+},{
+  image: 'assets/img/sra9.jpg',
+  thumbImage: 'assets/img/sra9.jpg',
+},{
+  image: 'assets/img/sra10.jpg',
+  thumbImage: 'assets/img/sra10.jpg',
+},{
+  image: 'assets/img/sra11.jpg',
+  thumbImage: 'assets/img/sra11.jpg',
+},{
+  image: 'assets/img/sra12.jpg',
+  thumbImage: 'assets/img/sra12.jpg',
+},{
+  image: 'assets/img/sra13.jpg',
+  thumbImage: 'assets/img/sra13.jpg',
+},{
+  image: 'assets/img/sra14.jpg',
+  thumbImage: 'assets/img/sra14.jpg',
+},{
+  image: 'assets/img/sra15.jpg',
+  thumbImage: 'assets/img/sra15.jpg',
+},
+];
+
+makeUp = [{
+  image: 'assets/img/m1.jpg',
+  thumbImage: 'assets/img/m1.jpg',
+},
+{
+  image: 'assets/img/m2.jpg',
+  thumbImage: 'assets/img/m2.jpg',
+},
+{
+  image: 'assets/img/m3.jpg',
+  thumbImage: 'assets/img/m3.jpg',
+},{
+  image: 'assets/img/m4.jpg',
+  thumbImage: 'assets/img/m4.jpg',
+},{
+  image: 'assets/img/m5.jpg',
+  thumbImage: 'assets/img/m5.jpg',
+},{
+  image: 'assets/img/m6.jpg',
+  thumbImage: 'assets/img/m6.jpg',
+},{
+  image: 'assets/img/m7.jpg',
+  thumbImage: 'assets/img/m7.jpg',
+},{
+  image: 'assets/img/m8.jpg',
+  thumbImage: 'assets/img/m8.jpg',
+},{
+  image: 'assets/img/m9.jpg',
+  thumbImage: 'assets/img/m9.jpg',
+},{
+  image: 'assets/img/m10.jpg',
+  thumbImage: 'assets/img/m10.jpg',
+},
+];
+
+hairCuts = [{
+  image: 'assets/img/h1.jpg',
+  thumbImage: 'assets/img/h1.jpg',
+},
+{
+  image: 'assets/img/h2.jpg',
+  thumbImage: 'assets/img/h2.jpg',
+},
+{
+  image: 'assets/img/h3.jpg',
+  thumbImage: 'assets/img/h3.jpg',
+},{
+  image: 'assets/img/h4.jpg',
+  thumbImage: 'assets/img/h4.jpg',
+},{
+  image: 'assets/img/h5.jpg',
+  thumbImage: 'assets/img/h5.jpg',
+},{
+  image: 'assets/img/h6.jpg',
+  thumbImage: 'assets/img/h6.jpg',
+},{
+  image: 'assets/img/h7.jpg',
+  thumbImage: 'assets/img/h7.jpg',
+},{
+  image: 'assets/img/h8.jpg',
+  thumbImage: 'assets/img/h8.jpg',
+},{
+  image: 'assets/img/h9.jpg',
+  thumbImage: 'assets/img/h9.jpg',
+},{
+  image: 'assets/img/h10.jpg',
+  thumbImage: 'assets/img/h10.jpg',
+},
 ];
 }
 
